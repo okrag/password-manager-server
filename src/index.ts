1cdbe35b02c013e00d6f2df60338de9591a57b56
Okrag <48834705+okrag@users.noreply.github.com>
2021-03-23 12:47:13 +0100

    Update port and cors origins

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ import { Server as SocketServer } from "socket.io";
 import cors from "cors";
 import ioconnection from "./ioconnection";
 
-const originList = ["chrome-extension://pldibipgckolnegeiigfekiebjlmopjm"];
+const originList = [
+  "chrome-extension://pldibipgckolnegeiigfekiebjlmopjm",
+  "https://192.168.0.102:8080",
+  "https://192.168.1.102:8080",
+  "https://25.65.222.22:8080",
+];
 
 const app = express();
 app.use(express.json());
@@ -20,6 +25,6 @@ import { router as PasswordsRouter } from "./passwords";
 app.use("/auth", AuthRouter);
 app.use("/passwords", PasswordsRouter);
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
 });
